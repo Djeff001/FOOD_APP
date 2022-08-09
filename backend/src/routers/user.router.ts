83fe09a3c608ce7add9a6deec45aac1072b7f3e2
Djeff <nait.sider.djaffer@gmail.com>
2022-08-09 02:27:21 +0200

    ge data from database login page

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import { sample_users } from "../data";
 import asyncHandler from "express-async-handler";
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
 
 const router = Router();
 
@@ -19,20 +19,21 @@ router.get(
   })
 );
 
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
-  const user = sample_users.find(
-    (user) => user.email === email && user.password === password
-  );
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    res.status(400).send("User name or password is not valid!");
-  }
-});
+    const user = await UserModel.findOne({ email, password });
+    if (user) {
+      res.send(generateTokenResponse(user));
+    } else {
+      res.status(400).send("User name or password is not valid!");
+    }
+  })
+);
 
-const generateTokenResponse = (user: any) => {
+const generateTokenResponse = (user: User) => {
   const token = jwt.sign(
     {
       email: user.email,
@@ -43,8 +44,15 @@ const generateTokenResponse = (user: any) => {
       expiresIn: "30d",
     }
   );
-  user.token = token;
-  return user;
+
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    address: user.address,
+    isAdmin: user.isAdmin,
+    token: token,
+  };
 };
 
 export default router;
