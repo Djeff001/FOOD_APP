517b76d40274030884021854f01f800bdc8c6120
Djeff <nait.sider.djaffer@gmail.com>
2022-08-09 13:29:36 +0200

    add register page and  its configuration

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import { sample_users } from "../data";
 import asyncHandler from "express-async-handler";
 import { User, UserModel } from "../models/user.model";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
@@ -33,7 +34,32 @@ router.post(
   })
 );
 
-const generateTokenResponse = (user: User) => {
+router.post(
+  "/register",
+  asyncHandler(async (req, res) => {
+    const { name, email, password, address } = req.body;
+
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      res.status(400).send("User is already exist, please login!");
+      return;
+    }
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser: User = {
+      id: "",
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      address,
+      isAdmin: false,
+    };
+    // id will be generated
+    const dbUser = UserModel.create(newUser);
+    res.send(generateTokenResponse(dbUser));
+  })
+);
+
+const generateTokenResponse = (user: any) => {
   const token = jwt.sign(
     {
       email: user.email,
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,8 +2,9 @@ import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { USER_LOGIN_URL } from '../shared/constants/urls';
+import { USER_LOGIN_URL, USER_REGISTER_URL } from '../shared/constants/urls';
 import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { IUserRegister } from '../shared/interfaces/IUserRegister';
 import { User } from '../shared/models/User';
 
 @Injectable({
@@ -33,6 +34,25 @@ export class UserService {
         },
         error: (errorResponse) => {
           this.toastrService.error(errorResponse.error, 'Login Failed');
+        },
+      })
+    );
+  }
+
+  register(userRegister: IUserRegister): Observable<User> {
+    return this.http.post<User>(USER_REGISTER_URL, userRegister).pipe(
+      tap({
+        next: (user) => {
+          this.setUserToLocalStorage(user);
+          //notify all the observable
+          this.userSubject.next(user);
+          this.toastrService.success(
+            'welcome to Foodmine ' + user.name,
+            'Register Successful'
+          );
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Register Failed');
         },
       })
     );
