67fb21f68775129ab13ca8658930e7cd611fc38e
Djeff <nait.sider.djaffer@gmail.com>
2022-08-11 01:19:04 +0200

    adding payment page

---
+++ frontend/src/app/components/partials/paypal-button/paypal-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Order } from 'src/app/shared/models/Order';
+
+@Component({
+  selector: 'paypal-button',
+  templateUrl: './paypal-button.component.html',
+  styleUrls: ['./paypal-button.component.css'],
+})
+export class PaypalButtonComponent implements OnInit {
+  @Input()
+  order!: Order;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
